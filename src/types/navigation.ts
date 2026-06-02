58168d0e74caa4d0c06d3928c53e7331451cc962
Ana Flávia V. Kunzendorff <avieirakunzen@gmail.com>
2026-06-01 22:42:16 -0300

    types: define react-navigation param lists

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -14,3 +14,28 @@ export type ScreenId =
   | 'change-password'
   | 'notifications'
   | 'about';
+
+export type AuthStackParamList = {
+  Welcome: undefined
+  Login: undefined
+  RegisterEmail: undefined
+  RegisterPassword: undefined
+}
+
+export type MainTabParamList = {
+  Home: undefined
+  Register: undefined
+  Maps: undefined
+  History: undefined
+  Profile: undefined
+}
+
+export type RootStackParamList = {
+  AuthStack: undefined
+  MainTabs: undefined
+  RecordDetail: { recordId: number }
+  EditProfile: undefined
+  ChangePassword: undefined
+  Notifications: undefined
+  About: undefined
+}
